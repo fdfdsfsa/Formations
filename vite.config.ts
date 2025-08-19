3363743be025a0e971787b04d3dac8e5296cbc58
fdfdsfsa <131728660+fdfdsfsa@users.noreply.github.com>
2025-08-19 19:26:44 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '',
+  base: '/Formations/',   // 👈 IMPORTANT for GitHub Pages
   plugins: [react()]
 })
+

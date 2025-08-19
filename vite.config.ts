e70ab6bc3ee3e6f2daf30351a840bace4e73005a
fdfdsfsa <131728660+fdfdsfsa@users.noreply.github.com>
2025-08-19 19:47:03 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Formations/',
+  base: '/Formations/',  // must match the repo name exactly (case-sensitive)
   plugins: [react()]
 })
